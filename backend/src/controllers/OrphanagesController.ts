3030abc23df438dd720ca04e6f4015f81c61e3c0
Philipe Galdino <philipep.galdino@gmail.com>
2020-10-19 18:05:04 -0300

    set up controllers with the routes

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,42 @@
+import { getRepository } from 'typeorm'
+import { Request, Response } from 'express'
+
+import Orphanage from '../models/Orphanage'
+
+export default {
+    async index(request: Request, response: Response) {
+        const orphanagesRepository = getRepository(Orphanage)
+
+        const orphanages = await orphanagesRepository.find()
+
+        return response.json(orphanages)
+    },
+
+    async create(request: Request, response: Response) {
+        const {
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends,
+        } = request.body
+
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    })
+
+    await orphanagesRepository.save(orphanage)
+
+    return response.status(201).json(orphanage)
+    }
+}
