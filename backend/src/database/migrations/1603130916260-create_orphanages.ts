75e20a2f82ca0637464965c88aa84df81de999ff
Philipe Galdino <philipep.galdino@gmail.com>
2020-10-19 17:49:20 -0300

    created orphanages model and ran its migration

--- backend/src/database/migrations/1603130916260-create_orphanages.ts
+++ backend/src/database/migrations/1603130916260-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1603130916260 implements MigrationInterface {
                     type: 'text',
                 },
                 {
+                    name: 'opening_hours',
+                    type: 'varchar'
+                },
+                {
                     name: 'open_on_weekends',
                     type: 'boolean',
                     default: false
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,7 @@
 import express from 'express'
+import { getRepository } from 'typeorm'
+
+import Orphanage from './models/Orphanage'
 
 import './database/connection'
 
@@ -6,7 +9,31 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/users', (req, res) => {
+app.post('/orphanages', async (req, res) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    } = req.body
+
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    })
+
+    await orphanagesRepository.save(orphanage)
+
     return res.json({ message: 'Hello there!' })
 })
 
