4062f0fce984a03ff1b5b519771e3a515d1ddb04
Philipe Galdino <philipep.galdino@gmail.com>
2020-10-23 02:48:46 -0300

    added a handler for the way frontend can view the data

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm'
 import { Request, Response } from 'express'
 
+import orphanageView from '../views/orphanages_view'
 import Orphanage from '../models/Orphanage'
 
 export default {
@@ -11,7 +12,7 @@ export default {
             relations: ['images']
         })
 
-        return response.json(orphanages)
+        return response.json(orphanageView.renderMany(orphanages))
     },
 
     async show(request: Request, response: Response) {
@@ -23,7 +24,7 @@ export default {
             relations: ['images']
         })
 
-        return response.json(orphanage)
+        return response.json(orphanageView.render(orphanage))
     },
 
     async create(request: Request, response: Response) {
